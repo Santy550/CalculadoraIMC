b4b473abd98110261c4515ad366c98ac86b4095f
santy <santyvegarajr@gmail.com>
2022-02-04 18:15:02 +0100

    CalculadoraIMC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from "@angular/forms";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { HistorialComponent } from './historial/historial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent,
+    HistorialComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.scss']
+})
+export class CalculadoraComponent implements OnInit {
+
+  calculadora = 'NOMBRE';
+
+  /*operandoA:number;
+  operandoB:number;
+  resultado:number;
+
+  onSumar():void {
+    this.resultado = this.operandoA + this.operandoB;
+  }*/
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
